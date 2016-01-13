29df4d582f8fc3c0bf3d84395b7741d836907760
sfgannon <sgannonumd@gmail.com>
2016-01-13 01:28:20 -0500

    Added chartist subsite and charts subsite
    Added charts subsites for reference and storage

---
+++ chartist/server.js
@@ -0,0 +1,12 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+var mongoose = require('mongoose'),
+express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+var port = process.env.PORT || 3001;
+
+app.listen(port);
+console.log("Express app running on " + port);
