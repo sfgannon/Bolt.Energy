f41ead87d7ec85687595b8e40de7afa239812296
Sean <sgannonumd@gmail.com>
2016-04-15 22:15:18 -0400

    Markup Templates

---
+++ MarkupTest/config/config.js
@@ -0,0 +1,27 @@
+var boltDB = require('mongoose');
+
+var config = (function() {
+	var dbHost = 'localhost';
+	var dbPort = '27017';
+	var dbName = 'boltDb';
+	var dbAuth = false;
+	var dbUserName = '';
+	var dbPassword = '';
+	var appPort = 3002;
+
+	return {
+		getDB: function() {
+			if (dbAuth) {
+				boltDB.connect('mongodb://' + dbUserName + ':' + dbPassword + dbHost + ':' + dbPort + '/' + dbName);
+			} else {
+				boltDB.connect('mongodb://' + dbHost + ':' + dbPort + '/' + dbName);
+			};
+			return boltDB;
+		},
+		getPort: function() {
+			return appPort;
+		}
+	};
+})(boltDB);
+
+module.exports = config;
