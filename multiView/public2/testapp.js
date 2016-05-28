d050959faf8b91b1a0a7022db7cde2f899eea374
sfgannon <sgannonumd@gmail.com>
2016-05-28 03:42:27 -0400

    Register, Login working. Directive and page
    Need to integrate FB and Google, need to fold directive into standard
    markup, or need to add a page to handle.
    Include directive in main page content, use a separate whole page for
    logging in adn registering, then when user navigates the directive will
    render correctly.

--- multiView/public2/testapp.js
+++ multiView/public2/testapp.js
@@ -13,18 +13,26 @@ angular.module("testapp", ["ui.router","ngResource"])
         controller: 'HomeController'
     })
  })
-  .controller('HomeController', ['$scope', function($scope){
+  .controller('HomeController', ['$scope','LoginService', function($scope,LoginService){
   	$scope.message = "Here's a message.";
+    $scope.register = function() {
+      var register = LoginService.register($scope.username, $scope.password);
+      register.then(function(response) {
+        $scope.message = "Registration Successful.";
+      }, function(status) {
+        $scope.message = "Registration failed: " + status;
+      })
+    }
  }])
-  .directive('authdirective', function(){
+  .directive('authdirective', ['LoginService', function(){
   	// Runs during compile
   	return {
   		//name: 'authdirective',
   		priority: 1,
   		// terminal: true,
   		scope: {}, // {} = isolate, true = child, false/undefined = no change
-  		controller: function($scope, $element, $attrs, $transclude) {
-  			$scope.authenticated = false;
+  		controller: function($scope, $element, $attrs, $transclude, LoginService) {
+  			$scope.authenticated = LoginService.authenticated();
   			$scope.login = function(username, password) {
   				console.log($scope.username + ' ' + $scope.password);
   				$scope.authenticated = true;
@@ -32,6 +40,17 @@ angular.module("testapp", ["ui.router","ngResource"])
   			$scope.logout = function() {
   				$scope.authenticated = false;
   			}
+        $scope.authenticate = function() {
+            var login = LoginService.login($scope.username, $scope.password);
+            login.then(function(data){
+                $scope.authenticated = true;
+                $state.go('home');
+                $scope.test = "Not fun.";
+            }, function() {
+                $scope.authenticated = false;
+                $state.go('login');
+            });
+        }
   		},
   		//require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
   		restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
@@ -44,4 +63,69 @@ angular.module("testapp", ["ui.router","ngResource"])
   			
   		}
   	};
-  });
+  }])
+  .service('ConfigService', ['$http', '$q', function($http,$q) {
+    return {
+      appRoot: function() {
+        var url = 'http://localhost:3002';
+        return url;
+      },
+      appPort: function() {
+        var port = 3002;
+        return port;
+      }
+    }
+  }])
+  .service('LoginService',['$http','$q','$window','ConfigService',function($http,$q,$window,ConfigService) {
+      return {
+          login: function(username,password) {
+              var $return = $q.defer();
+              $return.promise = $http({
+                method: 'POST',
+                url: ConfigService.appRoot() + '/data/authenticate',
+                data: {
+                    email: username,
+                    password: password
+                }
+              }).then(function(responseData) {
+                  $window.localStorage.setItem('BoltToken', responseData.token);
+                  $return.resolve({ token: responseData.token });
+              }, function(httpError) {
+                  $return.reject({ status: httpError.status })
+              })
+              return $return.promise;
+          },
+          logout: function() {
+              try {
+                  $window.localStorage.removeItem('BoltToken');
+              } catch (e) {
+                  console.log(e);
+              }
+          },
+          authenticated: function() {
+            if ($window.localStorage.getItem('BoltToken')) {
+                  return true;
+              } else {
+                  return false;
+              }
+          },
+          register: function(username, password) {
+            //Register a new user account
+            var $return = $q.defer();
+            $return.promise = $http({
+              method: 'POST',
+              url: ConfigService.appRoot() + '/data/authenticate',
+              data: {
+                email: username,
+                password: password
+              }
+            }).then(function(responseData) {
+              $window.localStorage.setItem('BoltToken', responseData.token);
+              $return.resolve({ token: responseData.token });
+            }, function(status) {
+              $return.reject({ status: status.status });
+            })
+            return $return.promise;
+          }
+      }
+  }])
