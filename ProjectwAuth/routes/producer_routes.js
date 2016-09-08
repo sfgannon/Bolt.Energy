ef998158c062ca6700a90ebbe625610b51c84b5c
sgannonumd@gmail.com <sgannonumd@gmail.com>
2016-09-08 03:29:24 +0200

    Updates to producer routes

--- ProjectwAuth/routes/producer_routes.js
+++ ProjectwAuth/routes/producer_routes.js
@@ -48,39 +48,8 @@ const requireAuth = passport.authenticate('jwt', { session: false });
 
 module.exports = function(app) {
 	var router = express.Router();
-	router.get('/producer', function(req, res, next) {
-	    //Get all profiles, or if req.email then search for one user by email
-	    var queryString = req.query;
-	    if (Object.keys(queryString).length > 0) {
-	    // if (req.query) {
-	      var result = Producer.find();
-	      for (i = 0; i < Object.keys(req.query).length; i++) {
-	        var term = Object.keys(req.query)[i];
-	        var value = req.query[term];
-	        if (term && value) {
-	        	result.where(term).equals(value);
-	        }
-	      };
-	      result.populate('uploads').populate('projects').exec(function(err, profiles) {
-	        if (err) {
-	          res.status(500).json({ error: err });
-	        }
-	        if (Object.keys(profiles).length == 0) {
-	          res.status(200).json({ message: "No producer profiles found matching search criteria." });
-	        } else {
-	          res.json({ producers: producers });
-	        }
-	      })
-	    } else {
-	      Producer.find(function(err, profile){
-	        if (err) {
-	          console.log(err);
-	          res.json({ error: err });
-	        }
-	        res.status(200).json(producer);
-	      })
-	    }
-	  })
+
+
 	router.post('/producer', requireAuth, uploadFile.array('files'), function(req, res, next) {
 		try {
 			var producerData = (req.body.data)?(JSON.parse(req.body.data)):('');
@@ -111,9 +80,94 @@ module.exports = function(app) {
 		}
 	})
 
-	router.get('/producer/:id', function(req, res, next) {
+	router.put('/producer/:id', requireAuh, uploadFile.array('files'), function(req, res, next) {
+		//Use separate route for projects and images
 		try {
-			if (objectid.isValid(req.params.id)) {
+			//Check req.user._id and producer.owner._id, if they match then save
+			Producer.findById(req.params.id, functin(err, producer) {
+				if (err) {
+					res.status(400).json({ error: err });
+				} else {
+				//Check ownership of producer
+				if (req.user._id == producer.owner) {
+					//Allow the update
+					producer.save(function(err, producer) {
+						if (err) {
+							res.status(400).json({ error: JSON.stringify(err) });
+						}
+						res.status(200).json({ producer: producer });
+					})
+				} else {
+					//Return forbidden
+					res.status(403).json({ error: "Forbidden" });
+				}
+			})
+		} catch(e) {
+			res.status(500).json({ error: JSON.stringify(err) });
+		}
+	})
+
+
+
+
+
+
+
+
+	//Rewrite
+	router.get('/producer', function(req, res) {
+		//Get all projects, narrow by search, how to implement paging?
+		//TODO implement paging, we don;t want users requesting thousands of profiles
+    //Get all profiles, or if req.email then search for one user by email
+    var queryString = req.query;
+    if (Object.keys(queryString).length > 0) {
+    // if (req.query) {
+      var result = Producer.find();
+      for (i = 0; i < Object.keys(req.query).length; i++) {
+        var term = Object.keys(req.query)[i];
+        var value = req.query[term];
+        if (term && value) {
+        	result.where(term).equals(value);
+        }
+      };
+      result.populate('uploads').populate('projects').exec(function(err, profiles) {
+        if (err) {
+          res.status(500).json({ error: err });
+        }
+        if (Object.keys(profiles).length == 0) {
+          res.status(200).json({ message: "No producer profiles found matching search criteria." });
+        } else {
+          res.json({ producers: producers });
+        }
+      })
+    } else {
+      Producer.find(function(err, profile){
+        if (err) {
+          console.log(err);
+          res.json({ error: err });
+        }
+        res.status(200).json(producer);
+      })
+    }
+  })
+
+	router.post('/producer', requireAuth, function(req, res) {
+		//New producer profile, check Owner, assign to req.user
+		var p = new Producer(req.body);
+		p.owner = req.user._id;
+		p.save(function(err, producer) {
+			if (err) {
+				res.status(500).json({ error: JSON.stringify(err) });
+			} else {
+				//Producer saved successfully
+				res.status(200).json({ producer: prodcer, message: "Save successful"})
+			}
+		})
+	})
+
+	router.get('/producer/:id', function(req, res) {
+		try {
+			if (objectId.isValid(req.params.id)) {
 				Producer.findById(req.params.id).populate('uploads').populate('projects').exec(function(err, producer) {
 					if (err) {
 						res.status(400).json({error: err});
@@ -127,9 +181,84 @@ module.exports = function(app) {
 		}
 	})
 
-	router.put('/producer/:id', requireAuh, uploadFile.array('files'), function(req, res, next) {
+	router.put('/producer/:id', requireAuth, function(req, res) {
+		try {
+			if (objectId.isValid(req.params.id)) {
+				Producer.findById(req.params.id, function(err, producer) {
+					if (producer.owner == req.user._id) {
+						//This user owns the profile, enable update
+						Producer.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true }, function(err, newProducer) {
+							if (err) {
+								res.status(500).json({ error: JSON.stringify(err) });
+							} else {
+								res.status(200).json({ producer: newProducer, message: "Update successful" });
+							}
+						})
+					} else {
+						res.status(403).json({ message: "Access denied" });
+					}
+				})
+			} else {
+				res.status(500).json({ message: "Invalid object id" });
+			}
+		} catch (e) {
+			res.status(500).json({ error: JSON.stringify(e) });
+		}
+	})
+
+	router.delete('/producer/:id', requireAuth, function(req, res) {
+		try {
+			if (objectId.isValid(req.params.id)) {
+				Producer.findById(req.params.id, function(err, producer) {
+					if (producer.owner == req.user._id) {
+						//This user owns the profile, enable update
+						Producer.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true }, function(err, newProducer) {
+							if (err) {
+								res.status(500).json({ error: JSON.stringify(err) });
+							} else {
+								res.status(200).json({ producer: newProducer, message: "Update successful" });
+							}
+						})
+					} else {
+						res.status(403).json({ message: "Access denied" });
+					}
+				})
+			} else {
+				res.status(500).json({ message: "Invalid object id" });
+			}
+		} catch (e) {
+			res.status(500).json({ error: JSON.stringify(e) });
+		}
+	})
+
+	router.post('/producer/:id/uploads', requireAuth, function(res, res) {
 		try {
 
-		} catch(e) 
+		} catch (e) {
+			res.status(404)
+		}
 	})
+
+	router.put('/producer/:id/uploads/:uploadId', requireAuth, function(req, res) {
+
+	})
+
+	router.delete('/producer/:id/uploads/:uploadId', requireAuth, function(req, res) {
+
+	})
+
+	router.post('/producer/:id/projects', requireAuth, function(res, res) {
+		
+	})
+
+	router.put('/producer/:id/projects/:projectId', requireAuth, function(req, res) {
+
+	})
+
+	router.delete('/producer/:id/projects/:projectId', requireAuth, function(req, res) {
+
+	})
+
+
+
 }
